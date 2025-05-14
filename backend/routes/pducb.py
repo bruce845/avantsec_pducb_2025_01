2ef9062553f5dfd5ba8314fe477d523da225e93c
Bruce de Souza Campos <brucecampos@avantsec.com.br>
2025-05-14 10:16:28 -0300

    Adicionada a variável routes_class

--- backend/routes/pducb.py
+++ backend/routes/pducb.py
@@ -10,3 +10,5 @@ class PDUCBRoutes:
     @staticmethod
     def include_static_routes(api_object: FastAPI):
         api_object.mount('/pducb_2025_01/frontend/static', StaticFiles(directory='systems/pducb2025_01/frontend/static'), name='pducb_2025_01-frontend')
+
+routes_class = PDUCBRoutes
