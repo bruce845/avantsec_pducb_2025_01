28b391f8458021820e581e80ac729c9b207cbcf4
Bruce de Souza Campos <brucecampos@avantsec.com.br>
2025-05-14 10:52:09 -0300

    Adicionada rota inicial

--- backend/routes/pducb.py
+++ backend/routes/pducb.py
@@ -10,5 +10,9 @@ class PDUCBRoutes:
     @staticmethod
     def include_static_routes(api_object: FastAPI):
         api_object.mount('/pducb_2025_01/frontend/static', StaticFiles(directory='systems/pducb2025_01/frontend/static'), name='pducb_2025_01-frontend')
+    
+    @router.get("/pducb",description="PDUCB - Página Inicial",include_in_schema=False)
+    async def pducb_initial_route(request: Request):
+        return Response(status_code=308, headers={"Location": "/pducb_2025_01/frontend/static/index.html"})
 
 routes_class = PDUCBRoutes
